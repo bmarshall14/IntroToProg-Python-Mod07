78612908df67295f979f049c7d9ab89a0f983c1a
bmarshall14 <133686588+bmarshall14@users.noreply.github.com>
2023-05-31 16:04:50 -0700

    Update Assignment07.py

--- Assignment07.py
+++ Assignment07.py
@@ -1,6 +1,14 @@
+# ---------------------------------------------------------------------------- #
+# Title: Assignment 07
+# Description: Working with pickles and exceptions to store complex data
+# Programmer: bmarshall
+# Date: 29May2022
+# ChangeLog (Who,When,What): none
+# ---------------------------------------------------------------------------- #
+
 # load pickle
 import pickle
-import pprint
+
 
 # Processing  --------------------------------------------------------------- #
 class Recipe:  # Initialize the variables needed for the recipe class
@@ -10,24 +18,29 @@ class Recipe:  # Initialize the variables needed for the recipe class
         self.baking_time = baking_time
 
 
+def make_dictionary(recipe):  # Add recipe to dictionary
+    recipe_dict = {
+        'recipe_name': recipe.recipe_name,
+        'ingredients': recipe.ingredients,
+        'baking_time': recipe.baking_time
+    }
+    return recipe_dict
+
+
 def baking_with_pickles(recipe, file_name):  # write data obtained from recipe class to a binary file
     try:
         with open(file_name, "wb") as file:
             pickle.dump(recipe, file)
-        print("Recipe saved using a pickle.\n")
+        print("Recipe pickled.\n")
     except IOError as e:  # exception to notify user if data was unable to be saved
         print("Error occurred while saving recipe: {e}")
 
 
-# def pickle_to_text(recipe, file_name):  # convert pickle to a text file for user to view
-#   open(file_name, "w")
-#   pickle_data = pickle.dumps(recipe)
-#    print(f"\nRecipe saved as text to {file_name}")
-def pickle_to_text(recipe):
-    unpickle = pickle.load(open('recipe.db', "rb"))
-    with open('recipe.txt', "a") as f:
-        pprint.pprint(unpickle, stream=f)
-    print(f"\nRecipe saved as text to recipe.txt")
+def save_dictionary_to_text(recipe_dict, file_name):  # write data obtained from recipe class to a binary file
+    with open(file_name, "w") as file:
+        for key, value in recipe_dict.items():
+            file.write(key + ": " + str(value) + "\n")
+
 
 # Presentation (input/output) --------------------------------------------------------------- #
 def get_recipe():  # get data from user to pickle
@@ -51,19 +64,23 @@ def display_recipe(recipe):  # display recipe info back to user
     print(f"Name: {recipe.recipe_name}")
     print(f"Ingredients: {recipe.ingredients}")
     print(f"Baking Time: {recipe.baking_time} minutes")
+    print("\nYou can also view your recipe by searching for the following file: recipe.txt" + "\n\nHappy baking!")
 
 
 # Main body of script  --------------------------------------------------------------- #
 # Get recipe details from the user
 recipe = get_recipe()
 
-# Save recipe to a pickle
-baking_with_pickles(recipe, "recipe.db")
-baking_with_pickles(recipe, "recipe.txt")
+# Create dictionary with user recipe
+recipe_dict = make_dictionary(recipe)
+
+# Pickle recipe data
+baking_with_pickles(recipe_dict, "recipe.db")
 
 # Display data in pickle to user
 display_recipe(recipe)
 
-# Convert pickle to text file for user
-pickle_to_text(recipe)
+# Save dictionary to txt file
+save_dictionary_to_text(recipe_dict, "recipe.txt")
+
 
